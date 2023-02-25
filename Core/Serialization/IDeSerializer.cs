ed8b209fbed63567eb84e8a3c661b0a6c9880477
Pedram GANJEH HADIDI <pedram.ganjeh.hadidi@gmail.com>
2023-02-25 15:17:11 +0100

    Added GPL license header Added icons Added resource dictionary Implemented Models, ViewModels, IDeSerializers for json and xml

--- Core/Serialization/IDeSerializer.cs
+++ Core/Serialization/IDeSerializer.cs
@@ -1,4 +1,20 @@
-﻿
+﻿/***************************************************************************************
+* This file is part of GeNSIS.                                                         *
+*                                                                                      *
+* GeNSIS is free software: you can redistribute it and/or modify it under the terms    *
+* of the GNU General Public License as published by the Free Software Foundation,      *
+* either version 3 of the License, or any later version.                               *
+*                                                                                      *
+* GeNSIS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     *
+* PURPOSE. See the GNU General Public License for more details.                        *
+*                                                                                      *
+* You should have received a copy of the GNU General Public License along with GeNSIS. *
+* If not, see <https://www.gnu.org/licenses/>.                                         *
+****************************************************************************************/
+
+
+
 namespace GeNSIS.Core.Serialization
 {
     using GeNSIS.Core.Models;
--- Core/Serialization/JsonDeSerializer.cs
+++ Core/Serialization/JsonDeSerializer.cs
@@ -1,5 +1,22 @@
-﻿namespace GeNSIS.Core.Serialization
+﻿/***************************************************************************************
+* This file is part of GeNSIS.                                                         *
+*                                                                                      *
+* GeNSIS is free software: you can redistribute it and/or modify it under the terms    *
+* of the GNU General Public License as published by the Free Software Foundation,      *
+* either version 3 of the License, or any later version.                               *
+*                                                                                      *
+* GeNSIS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;  *
+* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR     *
+* PURPOSE. See the GNU General Public License for more details.                        *
+*                                                                                      *
+* You should have received a copy of the GNU General Public License along with GeNSIS. *
+* If not, see <https://www.gnu.org/licenses/>.                                         *
+****************************************************************************************/
+
+
+namespace GeNSIS.Core.Serialization
 {
+    using GeNSIS.Core.Models;
     using Newtonsoft.Json;
 
     public class JsonDeSerializer : IDeSerializer
